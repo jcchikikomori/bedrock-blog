4ab691e3f911a96e74e003e6eb7b617f41fe3a5e
jcchikikomori <jccorsanes@protonmail.com>
2020-08-30 17:03:57 +0800

    refactor(capistrano): re-added npm for compiling current theme

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,8 @@ set :deploy_to, '/var/www/html'
 set :log_level, :info
 set :pty, true
 
-set :linked_dirs, %w{media web/app}
-set :linked_files, %w{.env web/.user.ini}
+set :linked_dirs, %w{media web/app/cache web/app/mu-plugins web/app/nfwlog web/app/themes web/app/updraft web/app/uploads web/app/w3tc-config}
+set :linked_files, %w{.env web/.user.ini web/app/advanced-cache.php}
 
 set :theme_path, "#{release_path}/web/app/themes/jcc-blog-2020-theme"
 
@@ -32,7 +32,15 @@ namespace :deploy do
     end
   end
 
+  desc 'Build theme'
+  task :build_theme do
+    on roles(:app) do
+      execute "cd #{fetch(:theme_path)} && npm install && npm run prod"
+    end
+  end
+
 end
 
 # before 'deploy:updated', 'grunt'
-after 'deploy', 'deploy:restart'
+after 'deploy', 'deploy:build_theme'
+after 'deploy:build_theme', 'deploy:restart'
